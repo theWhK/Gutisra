5635e803a2968203a35bc9969bc32704cc2a2e9e
Ulisses de Souza Melo <ulisses.usm@gmail.com>
2019-11-02 20:35:58 -0300

    aeHOO 2

--- load.py
+++ load.py
@@ -12,12 +12,14 @@ with np.load('knn_data.npz') as data:
 knn = cv2.ml.KNearest_create()
 knn.train(train, cv2.ml.ROW_SAMPLE, train_labels) 
   
-test_img=cv2.imread("test.png")
+test_img=cv2.imread("imagens/4-4.png")
 test_img =cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)
 test_img =cv2.resize(test_img, (20, 20))
 x = np.array(test_img)
 test_img = x.reshape(-1,400).astype(np.float32)
-ret,result,neighbours,dist = knn.findNearest(test_img,k=1)
+ret,result,neighbours,dist = knn.findNearest(test_img,k=5)
 
 #Printa o predict
-print int(result)
+print result
+print neighbours
+print dist
--- main.py
+++ main.py
@@ -8,35 +8,32 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # quebra a imagem em 5000 celulas, de 20x20 cada
 cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
 
-# Numpy array - size (50,100,20,20)
+# Numpy array - tamanho (50,100,20,20)
 x = np.array(cells)
 
-# Training and testing train_data and test_data.
+# Training e testing.
 train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
 test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
 
-# Create labels for train and test data
+# Cria rótulos para os dados de teste e treino
 k = np.arange(10)
 train_labels = np.repeat(k,250)[:,np.newaxis]
 test_labels = train_labels.copy()
 
-# Initiate kNN, train the data, then test it with test data for k=1
+# inicia o knn, treina e testa para k=5
 knn = cv2.ml.KNearest_create()
 knn.train(train, cv2.ml.ROW_SAMPLE,train_labels)
 ret, result, neighbours, dist = knn.findNearest(test, k=5)
 
 
-# Now we check the accuracy of classification
-# For that, compare the result with test_labels and check which are wrong
+# Checa a accuracy da classificação
+# Comparando os resultados com test_labels e checa qual está errado
 matches = result==test_labels
 correct = np.count_nonzero(matches)
 accuracy = correct*100.0/result.size
-<<<<<<< HEAD
 print accuracy
 
 # save the kNN Model
 np.savez('knn_data.npz',train=train, train_labels=train_labels)
 print "Saved"
-=======
-print(accuracy)
->>>>>>> ibagens
+
